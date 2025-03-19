c026b186560460aa2168d2f3e15941fd0009ac04
Eic17H <eic17h+github@gmail.com>
2025-03-19 13:16:19 +0100

    Introduzione a Vue

--- Lab/20250319/CineVUE/CineVUE/src/views/ContactsView.vue
+++ Lab/20250319/CineVUE/CineVUE/src/views/ContactsView.vue
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Cineva</title>
-        <meta name="author" content="Lorenzo Derosas"/>
-        <meta name="description" content="Contatti del sito web per recensire film"/>
-        <meta name="keywords" content="film, serie tv, recensioni, cinema"/>
-    </head>
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/Header.vue'
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
     <body>
-        <h1>Cineva - Contatti</h1>
-        <a href="index.html">Torna alla pagina iniziale</a>
+        <Header/>
         <p>Se avete bisogno di qualunque tipo di assiestenza potete contattarci ai seguenti indirizzi:</p>
         <p><b>Segnelazione utente:</b> <i>segnalazione.utentiburdi@cineva.it</i></p>
         <p><b>Richiesta nuova password:</b> <i>sono.sconato@cineva.it</i></p>
@@ -19,4 +17,4 @@
         <br>Palazzo delle Scienze
         <br>Via Ospedale 72, 09124, Cagliari CA</p>
     </body>
-</html>
+</template>
